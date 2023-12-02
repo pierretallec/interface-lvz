0af274a135a3126ff692544cefb5083b9972e873
pierretallec <pierre.tallec01@gmail.com>
2023-12-02 13:59:07 +0000

    Update api.js

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,3 +1,7 @@
+/** @format */
+
+import "isomorphic-fetch";
+
 import { apiURL } from "../config";
 
 class api {
@@ -10,51 +14,6 @@ class api {
   }
   getToken() {
     return this.token;
-  }
-
-  esQuery(queries) {
-    let query = "";
-    for (let i = 0; i < queries.length; i++) {
-      query += `${JSON.stringify(queries[i])}\n`;
-    }
-
-    return fetch(`${apiURL}/es/_msearch`, {
-      mode: "cors",
-      method: "POST",
-      redirect: "follow",
-      referrer: "no-referrer",
-      headers: {
-        "Content-Type": "application/x-ndjson",
-        Authorization: `JWT ${this.token}`,
-      },
-      body: query,
-    })
-      .then((r) => r.json())
-      .catch((e) => {
-        console.log(e);
-      });
-  }
-
-  getTotal(response) {
-    return (response && response.hits && response.hits.total) || 0;
-  }
-
-  getHits(response) {
-    return (response && response.hits && response.hits.hits) || [];
-  }
-
-  getAggregations(response, name) {
-    if (response && response.aggregations && response.aggregations[name]) {
-      if (response.aggregations[name].buckets) {
-        let obj = {};
-        for (let i = 0; i < response.aggregations[name].buckets.length; i++) {
-          obj[response.aggregations[name].buckets[i].key] =
-            response.aggregations[name].buckets[i].doc_count;
-        }
-        return obj;
-      }
-    }
-    return {};
   }
 
   get(path) {
@@ -124,31 +83,7 @@ class api {
     });
   }
 
-  postFormData(path, file) {
-    let formData = new FormData();
-    console.log("file", file);
-    formData.append(file.name, file, file.name);
-    return new Promise(async (resolve, reject) => {
-      try {
-        console.log(`${apiURL}${path}`);
-        const response = await fetch(`${apiURL}${path}`, {
-          mode: "cors",
-          method: "POST",
-          credentials: "include",
-          headers: {},
-          body: formData,
-        });
-        const res = await response.json();
-        console.log("e", res);
-        resolve(res);
-      } catch (e) {
-        console.log("e", e);
-        reject(e);
-      }
-    });
-  }
-
-  remove(path) {
+  delete(path) {
     return new Promise(async (resolve, reject) => {
       try {
         const response = await fetch(`${apiURL}${path}`, {
@@ -184,10 +119,12 @@ class api {
 
         const res = await response.json();
         if (response.status !== 200) {
+          console.error("Error:", res);
           return reject(res);
         }
         resolve(res);
       } catch (e) {
+        console.error("Error:", e);
         reject(e);
       }
     });
